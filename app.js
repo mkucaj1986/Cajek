c99d2d42fc7246020f4c7e5500245f8ab0257dd5
mk <mkucaj86@gmail.com>
2016-03-24 15:48:19 +0100

    adding modules view engine setup

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 var express = require('express');
 var app = express();
 
+var http = require('http');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
