6acf8208ee32dcb768ddfb2da50a954828a400de
mk <mkucaj86@gmail.com>
2016-05-26 16:26:36 +0200

    scroll to element

--- src/client/js/layout/home/home.controller.js
+++ src/client/js/layout/home/home.controller.js
@@ -11,9 +11,12 @@
         console.log('home ctrl');
 
         vm.toTheTop = function() {
-            $document.scrollTopAnimated(0, 700).then(function() {
-                console && console.log('You just scrolled to the top!');
-            });
+            $document.scrollTopAnimated(0, 700).then(function() {});
+        };
+
+        vm.scrollToElement = function(element) {
+            var someElement = angular.element(document.getElementById(element));
+            $document.scrollToElementAnimated(someElement, 53);
         };
     }
 })();
