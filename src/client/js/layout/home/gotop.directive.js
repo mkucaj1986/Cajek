56f8c92deca66c41545ce7f259def7f56013751f
mk <mkucaj86@gmail.com>
2016-06-12 14:05:55 +0200

    go top directive

---
+++ src/client/js/layout/home/gotop.directive.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+    angular
+        .module('app.layout')
+        .directive('gotopDirective', gotopDirective);
+    /* @ngInject */
+    function gotopDirective($window) {
+        var directive = {
+            link: link,
+            restrict: 'EAC'
+        };
+        return directive;
+
+        function link(scope, element, attrs, vm) {
+            angular.element($window).bind("scroll", function() {
+                var currentScroll = $(this).scrollTop();
+                if (currentScroll >= 100) {
+                    scope.goUpArrow = true;
+                } else {
+                    scope.goUpArrow = false;
+                }
+                scope.$apply();
+            });
+        }
+    }
+})();
