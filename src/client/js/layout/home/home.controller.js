7a46371ccce5d8449b19c49081ab7a7f62a417d2
Cajek <mkucaj86@gmail.com>
2016-12-05 22:55:11 +0100

    birthday bind

--- src/client/js/layout/home/home.controller.js
+++ src/client/js/layout/home/home.controller.js
@@ -41,5 +41,17 @@
         vm.isActive = function(route) {
             return route === $location.url();
         };
+
+        vm.calculateAge = function(dateString) {
+            // birthday is a date
+            var today = new Date();
+            var birthDate = new Date(dateString);
+            var age = today.getFullYear() - birthDate.getFullYear();
+            var m = today.getMonth() - birthDate.getMonth();
+            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                age--;
+            }
+            return age;
+        };
     }
 })();
