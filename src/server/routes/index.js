daf16fd73897389161f1e91d712d2a25098b213b
Cajek <mkucaj86@gmail.com>
2017-04-03 17:13:03 +0200

    change email service name

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -11,7 +11,7 @@ router.get('/*', function(req, res, next) {
     };
     res.render('index.hbs', vm);
 });
-/* GET Contact page. */
+/* SEND EMAIL . */
 router.post('/contactForm', sendEmail);
 
 module.exports = router;
