1f9041bece10b1a237b82661ae4fd8d457ceabdd
mkucaj <mkucaj86@gmail.com>
2017-05-11 10:54:07 +0200

    change gmail services to sendinblue more easiest and quicker to setup

--- src/server/email/emailTransporter.js
+++ src/server/email/emailTransporter.js
@@ -1,16 +1,27 @@
+/*jshint esversion: 6 */
 const nodemailer = require('nodemailer');
 const config = require('../config/config');
+const sendinBlue = require('nodemailer-sendinblue-transport');
 
 let sendEmail = function(req, res, next) {
 
     if (!req.body) return res.sendStatus(400);
-    let transporter = nodemailer.createTransport(config.email.smtpConfig);
+    let transporter = nodemailer.createTransport(sendinBlue(config.email.sendinBlue));
     let mailOptions = {
         to: config.email.emailTo,
         from: req.body.email,
         subject: req.body.email,
         html: req.body.message
     };
+    // verify connection configuration
+    transporter.verify(function(error, success) {
+       if (error) {
+            console.log(error);
+            res.status(401).json({ err: error });
+       } else {
+            console.log('Server is ready to take our messages');
+       }
+    });
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
             res.status(401).json({ err: info });
@@ -21,6 +32,7 @@ let sendEmail = function(req, res, next) {
             next();
         }
         res.send(info);
+        transporter.close();
     });
 };
 module.exports = sendEmail;
