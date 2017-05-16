b2d74280750e6f4225a53172b1bab523901824c5
Cajek <mkucaj86@gmail.com>
2017-05-17 00:01:09 +0200

    fix sections height on devices

--- src/client/js/layout/home/gotop.directive.js
+++ src/client/js/layout/home/gotop.directive.js
@@ -2,7 +2,8 @@
     'use strict';
     angular
         .module('app.layout')
-        .directive('gotopDirective', gotopDirective);
+        .directive('gotopDirective', gotopDirective)
+        .directive('resizeDir', resizeDir);
     /* @ngInject */
     function gotopDirective($window) {
         var directive = {
@@ -19,6 +20,40 @@
                 } else {
                     scope.goUpArrow = false;
                 }
+                scope.$apply();
+            });
+        }
+    }
+
+    function resizeDir($window, $document) {
+        var directive = {
+            link: link,
+            restrict: 'EAC'
+        };
+        return directive;
+
+        function link(scope, element, attrs, vm) {
+            var w = angular.element($window);
+            scope.getWindowDimensions = function() {
+                return {
+                    'h': w.height(),
+                    'w': w.width()
+                };
+            };
+            scope.$watch(scope.getWindowDimensions, function(newValue, oldValue) {
+                scope.windowHeight = newValue.h;
+                scope.windowWidth = newValue.w;
+
+                scope.style = function() {
+                    return {
+                        'height': newValue.h + 'px',
+                        // 'width': newValue.w + 'px'
+                    };
+                };
+
+            }, true);
+
+            w.bind('resize', function() {
                 scope.$apply();
             });
         }
