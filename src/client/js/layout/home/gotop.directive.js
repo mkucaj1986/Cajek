c90c73deced4b58a5f72c5e3f3305524696b7967
mkucaj <mkucaj86@gmail.com>
2017-05-18 15:22:59 +0200

    split resize dir into new file

--- src/client/js/layout/home/gotop.directive.js
+++ src/client/js/layout/home/gotop.directive.js
@@ -2,8 +2,8 @@
     'use strict';
     angular
         .module('app.layout')
-        .directive('gotopDirective', gotopDirective)
-        .directive('resizeDir', resizeDir);
+        .directive('gotopDirective', gotopDirective);
+        
     /* @ngInject */
     function gotopDirective($window) {
         var directive = {
@@ -25,37 +25,4 @@
         }
     }
 
-    function resizeDir($window, $document) {
-        var directive = {
-            link: link,
-            restrict: 'EAC'
-        };
-        return directive;
-
-        function link(scope, element, attrs, vm) {
-            var w = angular.element($window);
-            scope.getWindowDimensions = function() {
-                return {
-                    'h': w.height(),
-                    'w': w.width()
-                };
-            };
-            scope.$watch(scope.getWindowDimensions, function(newValue, oldValue) {
-                scope.windowHeight = newValue.h;
-                scope.windowWidth = newValue.w;
-
-                scope.style = function() {
-                    return {
-                        'height': newValue.h + 'px',
-                        // 'width': newValue.w + 'px'
-                    };
-                };
-
-            }, true);
-
-            w.bind('resize', function() {
-                scope.$apply();
-            });
-        }
-    }
 })();
