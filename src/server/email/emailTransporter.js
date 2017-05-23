3838050d7d2c0bf78af127bebe4e31439b137dc4
mkucaj <mkucaj86@gmail.com>
2017-05-23 14:43:23 +0200

    move transporter verify

--- src/server/email/emailTransporter.js
+++ src/server/email/emailTransporter.js
@@ -2,26 +2,29 @@
 const nodemailer = require('nodemailer');
 const config = require('../config/config');
 const sendinBlue = require('nodemailer-sendinblue-transport');
+const transporter = nodemailer.createTransport(sendinBlue(config.email.sendinBlue));
+
+// verify connection configuration
+transporter.verify(function(error, success) {
+   if (error) {
+        console.log(error);
+        res.status(401).json({ err: error });
+   } else {
+        console.log('Server is ready to take our messages');
+   }
+});
 
 let sendEmail = function(req, res, next) {
 
     if (!req.body) return res.sendStatus(400);
-    let transporter = nodemailer.createTransport(sendinBlue(config.email.sendinBlue));
+
     let mailOptions = {
         to: config.email.emailTo,
         from: req.body.email,
         subject: req.body.email,
         html: req.body.message
     };
-    // verify connection configuration
-    transporter.verify(function(error, success) {
-       if (error) {
-            console.log(error);
-            res.status(401).json({ err: error });
-       } else {
-            console.log('Server is ready to take our messages');
-       }
-    });
+
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
             res.status(401).json({ err: info });
